f4a75689ea2daffa110ebacbfac5b41f9715978b
Donodapadoca <joao.timao@hotmail.com>
2023-11-13 17:23:58 -0300

    fim

--- src/assets/Meucontador.jsx
+++ src/assets/Meucontador.jsx
@@ -1,35 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
+export default function Meucontador() {
+  const [contador, setContador] = useState(0);
 
-export default function Meucontador(){
+  function dobrar() {
+    setContador(contador * 2);
+  }
 
+  function aumentar() {
+    setContador(contador + 1);
+  }
 
-const [contador, setContador] = useState(0)
+  function diminuir() {
+    setContador(contador - 1);
+  }
+  function zerar() {
+    setContador(contador - contador);
+  }
 
-function dobrar(){
-    setContador(contador * 2)
-}
-
-function aumentar(){
-    setContador(contador + 1)
-}
-
-function diminuir(){
-    setContador(contador -1)
-}
-function zerar(){
-    setContador(contador - contador)
-}
-
-return(
+  return (
     <div>
-        <h1>Meu Contador</h1>
-        <h3>{contador}</h3>
-        <button onClick={aumentar}>aumentar</button>
-        <button onClick={diminuir}>diminuir</button>
-        <button onClick={dobrar}>dobrar</button>
-        <button onClick={zerar}>zerar</button>
-    </div>
-)
+      <h1>Meu Contador</h1>
+      {contador > 100 ? <h2>então ta amigao tu passou do 100</h2> : null}
+      {contador > 1000 ? <h2>então ta amigao tu passou do 1000</h2> : null}
+      {contador > 10000 ? <h2>então ta amigao tu passou do 10000</h2> : null}
 
+      <h3>{contador}</h3>
+      <button onClick={aumentar}>aumentar</button>
+      <button onClick={diminuir}>diminuir</button>
+      <button onClick={dobrar}>dobrar</button>
+      <button onClick={zerar}>zerar</button>
+    </div>
+  );
 }
