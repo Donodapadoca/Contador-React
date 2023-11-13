cbae39810c2d48495938792a07e09f3bea94e9f2
Donodapadoca <joao.timao@hotmail.com>
2023-11-13 19:38:01 -0300

    styles do css

--- src/assets/Meucontador.jsx
+++ src/assets/Meucontador.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from './button.module.css'
 
 export default function Meucontador() {
   const [contador, setContador] = useState(0);
@@ -19,17 +20,17 @@ export default function Meucontador() {
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>Meu Contador</h1>
       {contador > 100 ? <h2>então ta amigao tu passou do 100</h2> : null}
       {contador > 1000 ? <h2>então ta amigao tu passou do 1000</h2> : null}
       {contador > 10000 ? <h2>então ta amigao tu passou do 10000</h2> : null}
 
       <h3>{contador}</h3>
-      <button onClick={aumentar}>aumentar</button>
-      <button onClick={diminuir}>diminuir</button>
-      <button onClick={dobrar}>dobrar</button>
-      <button onClick={zerar}>zerar</button>
+      <button onClick={aumentar} className={styles.myButton}>aumentar</button>
+      <button onClick={diminuir}className={styles.myButton}>diminuir</button>
+      <button onClick={dobrar}className={styles.myButton}>dobrar</button>
+      <button onClick={zerar}className={styles.myButton}>zerar</button>
     </div>
   );
 }
